9fd4900abf50fdea2fb6e645332ab2167745e7d6
getsnaveen <getsnaveen@gmail.com>
2025-03-18 20:26:22 +0530

    updated static file path and updated path forward slashes

--- main.py
+++ main.py
@@ -13,28 +13,31 @@ warnings.filterwarnings("ignore")
 st.title(" Multilingual transcriber ")
 with st.form("my_form"):
     uploaded_file = st.text_input(label = "video path for movie title creation" , value = "")  
-    uploaded_file = r"F:\Data Science Pro\Multilingual-Transcriber\Input\9.mp4"
-    print("#######", uploaded_file)
+    # uploaded_file = r"F:\Data Science Pro\Multilingual-Transcriber\Input\9.mp4"
+    # print("#######", uploaded_file)
     submitted = st.form_submit_button("Submit")    
     base_name = os.path.basename(uploaded_file)
     file_name_without_extension, _ = os.path.splitext(base_name)
-    print("#######",file_name_without_extension)
+    # print("#######",file_name_without_extension)
         
     if submitted:        
         output_filename = file_name_without_extension+ "_audiofile"
-        output_filename = "output\\audiofiles\\"+output_filename        
+        # output_filename = "output\\audiofiles\\"+output_filename     
+        output_filename = "output/audiofiles/"+output_filename   
         st.info("Audio extraction is in progress")
         output_filename = output_filename+".mp3"
         AudioExtractor.AudioExtraction(inputpath = uploaded_file, outputpath = output_filename)     
         st.info("Transcribe is inprogress ")
         srtfilename = file_name_without_extension+"_SRTfile"  
-        srtfilename = "output\\srtfiles\\"+srtfilename+".srt"                        
+        # srtfilename = "output\\srtfiles\\"+srtfilename+".srt"   
+        srtfilename = "output/srtfiles/"+srtfilename+".srt"                     
         AudioTranscriptor.AudioTranscriptiontoFile(inputpath=output_filename, languatetoconvert= "en",outputpath = srtfilename)
         st.info("SRT file integration is in progress ")
         video = VideoFileClip(uploaded_file)
         subtitles = pysrt.open(srtfilename)
         output_final = file_name_without_extension+ "_subtitled.mp4"
-        output_final = "output\\final\\"+output_final 
+        # output_final = "output\\final\\"+output_final 
+        output_final = "output/final/"+output_final 
         
         print ("Output file name: ",output_final)
 
